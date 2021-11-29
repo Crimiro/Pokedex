03b46e9c4c44f0cd2aebbcb34482f97e74899351
Christian Michael Rosales <crimiro05@gmail.com>
2021-11-28 21:40:48 -0600

    Login/Logout simulated

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: 'xcrix5',
+  password: '123456'
+}
+
+export const userDetails = {
+  username: 'xcrix5',
+  firstName: 'Crix',
+  lastName: 'Rosales',
+  email: 'xcrix@gmail.com'
+}
